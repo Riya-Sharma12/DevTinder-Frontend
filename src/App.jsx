d33fd30ccf86990b4bb836f057b64567cfd9dca0
Riya-Sharma12 <sharmariya0163@gmail.com>
2025-05-04 18:02:13 +0530

    login-page

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,25 @@ import Body from "./components/body";
 import {BrowserRouter, Routes , Route} from "react-router-dom"  ;
 import Login from "./components/login";
 import Profile from "./components/profile";
+import appStore from "./utils/appStore";
+import { Provider } from "react-redux";
+import Feed from "./components/feed";
 function App() {
   return (
 <>
+<Provider store={appStore}>
 <BrowserRouter basename="/">
 <Routes>
 <Route path="/" element= {<Body></Body>}>
+<Route path="/" element={<Feed/>} />
 <Route path="/login" element={<Login></Login>} />
 <Route path="/profile" element={<Profile></Profile>} />
-{/* <Route path="/" element={} />
-<Route path="/" element={} />
-<Route path="/" element={} /> */}
+
 </Route>
 </Routes>
 </BrowserRouter>
+</Provider>
 </>
   )
-}
+} 
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
+  const user = useSelector((store) => store.user);
+  console.log(user);
   return (
     <div className="navbar bg-base-300 ">
     <div className="flex-1">
       <a className="btn btn-ghost text-xl">👨🏼‍💻DevTinder</a>
     </div>
-    <div className="flex gap-2">
+    {user && <div className="flex gap-2">
       <div className="dropdown dropdown-end mx-5">
+        <p>👋 Welcome, {user.firstName} !</p>
         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-          <div className="w-10 rounded-full">
+        {user &&   <div className="w-10 rounded-full">
             <img
-              alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-          </div>
+              alt="User Avatar"
+              src={user.photoURL} />
+          </div>}
         </div>
         <ul
           tabIndex={0}
@@ -27,8 +31,8 @@ const Navbar = () => {
           <li><a>Settings</a></li>
           <li><a>Logout</a></li>
         </ul>
-      </div>
-    </div>
+      </div> 
+    </div> }
   </div>
   )
 }
